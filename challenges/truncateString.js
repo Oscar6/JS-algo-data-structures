0b7a7dba0ba1699bbd9376f3ba2b220f8615f220
Oscar6 <oscar.m1024@gmail.com>
2022-11-19 16:38:07 -0600

    truncate a string

--- challenges/truncateString.js
+++ challenges/truncateString.js
@@ -1,12 +1,12 @@
 // Truncate a String
 
 function truncateString(str, num) {
-// We start off with a simple if statement to determine one of two outcomes
-// If our string length is greater than the num we want to truncate it, 
-// we return a slice of our string starting at character 0, 
+// Create a simple if statement to determine one of two outcomes
+// If the string length is greater than the num, truncate it, 
+// return a slice of the string starting at character 0, 
 // and ending at num. 
     if (str.length > num) {
-    // We then append our '...' to the end of the string.
+    // Append '...' to the end of the string.
         return str.slice(0, num) + "...";
     // If not true, the string length is less than the truncation num. 
     } else {
