aedb17f947af5f7a121e28948684d8154e40079a
Oscar6 <oscar.m1024@gmail.com>
2022-11-22 21:06:49 -0600

    title case a sentence - fix

--- challenges/capitalizeLetters.js
+++ challenges/capitalizeLetters.js
@@ -6,7 +6,7 @@ function titleCase(str) {
 // Using for loop
     for (let i = 0; i < words.length; i++) {
 // Concatenate first character in each word with lowercase letters
-        words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1).toLowerCase();
     }
 // Join all words within string
 return words.join(" ");
